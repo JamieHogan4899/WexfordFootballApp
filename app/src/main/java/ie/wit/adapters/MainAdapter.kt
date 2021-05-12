5b84c69f0d11539cf8abfe741d6831e1d3ef9643
jamie <20084469@mail.wit.ie>
2021-05-12 20:55:05 +0100

    Final Version

--- app/src/main/java/ie/wit/adapters/MainAdapter.kt
+++ app/src/main/java/ie/wit/adapters/MainAdapter.kt
@@ -62,46 +62,4 @@ class MainAdapter(options: FirebaseRecyclerOptions<TeamModel>,
 
 
 
-
-   /* override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainHolder {
-        return MainHolder(
-            LayoutInflater.from(parent.context).inflate(
-                R.layout.card_register,
-                parent,
-
-                false
-            )
-        )
-    }
-    val reportAll = reportall
-
-
-    //note no listener so you cant click in the list screen
-    override fun onBindViewHolder(holder: MainHolder, position: Int) {
-        val team = teams[holder.adapterPosition]
-        holder.bind(team, listener, reportAll)
-
-    }
-
-    override fun getItemCount(): Int = teams.size
-
-    //show added teams
-    class MainHolder constructor(itemView: View) : RecyclerView.ViewHolder(itemView)  {
-        fun bind(team: TeamModel, listener: TeamListener, reportAll: Boolean) {
-            itemView.tag = team
-            itemView.imageIcon.setImageBitmap(readImageFromPath(itemView.context, team.image))
-            itemView.listName.text = team.name
-            itemView.teamLocation.text = team.location
-            itemView.teamAmount.text = team.amount.toString()
-            if(team.isfavourite) itemView.imagefavourite.setImageResource(android.R.drawable.star_big_on)
-            if(!reportAll)
-                itemView.setOnClickListener { listener.onTeamClick(team) }
-
-
-
-        }
-    }
-
-
-
-*/}
+}
